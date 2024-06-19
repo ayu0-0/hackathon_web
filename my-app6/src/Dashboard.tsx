50a38bf5cda0d186031eec1042839adfbb607fb8
ayu0-0 <ayuko09381002@icloud.com>
2024-06-19 11:21:51 +0900

    POSTtoliketable

--- my-app6/src/Dashboard.tsx
+++ my-app6/src/Dashboard.tsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect, MouseEventHandler } from 'react';
 import './App.css';
 import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom'
-import { fireAuth } from './firebaseAuth'; // 不要なインポートは削除
+import { Link } from 'react-router-dom';
+import { fireAuth } from './firebaseAuth';
 import likeImage from './images/like.png';
 import likedImage from './images/liked.png';
-
 
 interface User {
   id: string;
@@ -15,7 +14,7 @@ interface User {
 }
 
 interface Posts {
-  id: number;
+  id: string;
   user_id: string;
   content: string;
   created_at: Date;
@@ -25,20 +24,19 @@ const Contents: React.FC<{ signOut: () => void }> = ({ signOut }) => {
   const [loginUser, setLoginUser] = useState(fireAuth.currentUser);
   const [users, setUsers] = useState<User[]>([]);
   const [posts, setPosts] = useState<Posts[]>([]);
-  const [likedPosts, setLikedPosts] = useState<Set<number>>(new Set());
+  const [likedPosts, setLikedPosts] = useState<Set<string>>(new Set());
   const [userUid, setUserUid] = useState<string | null>(null);
+  const [viewAllPosts, setViewAllPosts] = useState(false);
 
   useEffect(() => {
     const unsubscribe = fireAuth.onAuthStateChanged(user => {
       setLoginUser(user);
       if (user) {
-        setUserUid(user.uid); // ログインユーザーのUIDをステートに設定
+        setUserUid(user.uid);
       } else {
         setUserUid(null);
       }
     });
-
-
 
     const fetchUsers = async () => {
       try {
@@ -57,17 +55,13 @@ const Contents: React.FC<{ signOut: () => void }> = ({ signOut }) => {
 
     const fetchPosts = async () => {
       try {
-        const response = await fetch(
-          "http://localhost:8000/posts",
-          {
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }
-        );
+        const response = await fetch("http://localhost:8000/posts", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
         const data = await response.json();
-        console.log('ここまでok')
         const postsWithDate = data.map((post: Posts) => ({
           ...post,
           created_at: new Date(post.created_at)
@@ -88,12 +82,10 @@ const Contents: React.FC<{ signOut: () => void }> = ({ signOut }) => {
 
   const handlePostButtonClick = (e: any) => {
     e.preventDefault();
-    console.log("navigate post")
-    navigate('/post'); // '/post'ページに遷移
+    navigate('/post');
   };
 
-
-  const handleLikeClick = (postId: number) => {
+  const handleLikeClick = (postId: string) => {
     setLikedPosts(prevLikedPosts => {
       const newLikedPosts = new Set(prevLikedPosts);
       if (newLikedPosts.has(postId)) {
@@ -103,6 +95,41 @@ const Contents: React.FC<{ signOut: () => void }> = ({ signOut }) => {
       }
       return newLikedPosts;
     });
+
+    fetchLike(postId);
+  };
+
+  const fetchLike = async (postId: string) => {
+    try {
+      const postResponse = await fetch("http://localhost:8000/likes", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ user_id: userUid, post_id: postId }),
+      });
+      if (!postResponse.ok) {
+        throw new Error('データの送信に失敗しました');
+      }
+    } catch (err) {
+      console.error(err);
+      return;
+    }
+
+    try {
+      const getResponse = await fetch("http://localhost:8000/posts");
+      if (!getResponse.ok) {
+        throw new Error('データの取得に失敗しました');
+      }
+      const getData = await getResponse.json();
+      const postsWithDate = getData.map((post: Posts) => ({
+        ...post,
+        created_at: new Date(post.created_at)
+      }));
+      setPosts(postsWithDate);
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   const getUserName = (userId: string) => {
@@ -111,33 +138,27 @@ const Contents: React.FC<{ signOut: () => void }> = ({ signOut }) => {
   };
 
   const filteredAndSortedPosts = posts
-    .filter(post => post.user_id === userUid)
-    .sort((a, b) => b.created_at.getTime() - a.created_at.getTime()); // 時系列順にソート
+    .filter(post => viewAllPosts || post.user_id === userUid)
+    .sort((a, b) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime());
 
   return (
     <div>
       <div className="App-header">
-        <p>My posts</p>
-        <button onClick={signOut}>
-          ログアウト
-        </button>
+        <button className="to-my-posts-button" onClick={() => setViewAllPosts(false)}>自分の投稿</button>
+        <button className="to-friends-posts-button" onClick={() => setViewAllPosts(true)}>友達の投稿</button>
+        <button onClick={signOut}>ログアウト</button>
       </div>
       <div>
         <ul className="list" style={{ listStyleType: 'none', padding: 0 }}>
           {filteredAndSortedPosts.map(filteredPost => (
             <li key={filteredPost.id} className="listItem">
-              {/* 投稿全体を囲むコンテナ */}
               <div className="postContainer">
-                {/* ユーザー名を表示 */}
                 <div className="userName">
                   {getUserName(filteredPost.user_id).name}
                   <span className="userId"> @{getUserName(filteredPost.user_id).userid}</span>
                 </div>
-                {/* コンテンツを表示 */}
                 <div className="content">{filteredPost.content}</div>
-                {/* 作成日時を表示（フォーマット） */}
                 <div className='postedAt'>{filteredPost.created_at.toLocaleString()}</div>
-                {/* ボタンとして画像を追加 */}
                 <button className="likeButton" onClick={() => handleLikeClick(filteredPost.id)}>
                   <img
                     src={likedPosts.has(filteredPost.id) ? likedImage : likeImage}
